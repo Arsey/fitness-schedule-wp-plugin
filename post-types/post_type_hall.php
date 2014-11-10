81d7d895c972ee84add737c375c076acc1b3b0d5
Arsey <arseysensector@gmail.com>
2014-11-10 18:33:06 +0200

    added city column for halls list in admin

--- post-types/post_type_hall.php
+++ post-types/post_type_hall.php
@@ -5,7 +5,8 @@ if (!class_exists('Post_Type_Hall')) {
     /**
      * A PostTypeHall class that provides 2 additional meta fields
      */
-    class Post_Type_Hall {
+    class Post_Type_Hall
+    {
 
         const POST_TYPE = "kivi_schedule_hall";
 
@@ -17,7 +18,8 @@ if (!class_exists('Post_Type_Hall')) {
         /**
          * The Constructor
          */
-        public function __construct() {
+        public function __construct()
+        {
             // register actions
             add_action('init', array(&$this, 'init'));
             add_action('admin_init', array(&$this, 'admin_init'));
@@ -28,7 +30,8 @@ if (!class_exists('Post_Type_Hall')) {
         /**
          * hook into WP's init action hook
          */
-        public function init() {
+        public function init()
+        {
             // Initialize Post Type
             $this->create_post_type();
             add_action('save_post', array(&$this, 'save_post'));
@@ -36,33 +39,37 @@ if (!class_exists('Post_Type_Hall')) {
 
 // END public function init()
 
+
         /**
          * Create the post type
          */
-        public function create_post_type() {
+        public function create_post_type()
+        {
             register_post_type(self::POST_TYPE, array(
-                'labels' => array(
-                    'name' => __('Halls'),
-                    'singular_name' => __('Hall'),
-                    'add_new' => __('Add Hall'),
-                    'view_item' => __('View'),
-                    'search_items' => __('Find Hall'),
-                    'add_new_item' => __('Add Hall')
-                ),
-                'public' => true,
-                'has_archive' => true,
-                'show_in_menu' => 'time_table',
-                'supports' => array(
-                    'title'
-                ),
-                    )
+                    'labels' => array(
+                        'name' => __('Halls'),
+                        'singular_name' => __('Hall'),
+                        'add_new' => __('Add Hall'),
+                        'view_item' => __('View'),
+                        'search_items' => __('Find Hall'),
+                        'add_new_item' => __('Add Hall')
+                    ),
+                    'public' => true,
+                    'has_archive' => true,
+                    'show_in_menu' => 'time_table',
+                    'capability_type' => 'post',
+                    'supports' => array(
+                        'title'
+                    ),
+                )
             );
         }
 
         /**
          * Save the metaboxes for this custom post type
          */
-        public function save_post($post_id) {
+        public function save_post($post_id)
+        {
             // verify if this is an auto save routine. 
             // If it is our form has not been submitted, so we dont want to do anything
             if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
@@ -84,20 +91,44 @@ if (!class_exists('Post_Type_Hall')) {
         /**
          * hook into WP's admin_init action hook
          */
-        public function admin_init() {
+        public function admin_init()
+        {
             // Add metaboxes
             add_action('add_meta_boxes', array(&$this, 'add_meta_boxes'));
+            add_filter('manage_edit_' . self::POST_TYPE . '_posts_custom_column', array(&$this, 'add_custom_columns'), 10, 2);
+
+            add_filter('manage_edit-' . self::POST_TYPE . '_columns', array(&$this, 'set_custom_edit_columns'));
+            add_action('manage_' . self::POST_TYPE . '_posts_custom_column', array(&$this, 'custom_column'), 10, 2);
         }
 
+        function set_custom_edit_columns($columns)
+        {
+            return array(
+                'cb' => $columns['cb'],
+                'title' => $columns['title'],
+                'club' => __('Club', 'kiwi_schedule_clubs'),
+                'date' => $columns['date']
+            );
+        }
+
+        function custom_column($column, $post_id)
+        {
+            switch ($column) {
+                case 'club' :
+                    echo get_the_title(get_post_meta($post_id, 'hall_city_id', true));
+                    break;
+            }
+        }
 // END public function admin_init()
 
         /**
          * hook into WP's add_meta_boxes action hook
          */
-        public function add_meta_boxes() {
+        public function add_meta_boxes()
+        {
             // Add this metabox to every selected post
             add_meta_box(
-                    sprintf('wp_plugin_template_%s_section', self::POST_TYPE), __('Hall'), array(&$this, 'add_inner_meta_boxes'), self::POST_TYPE
+                sprintf('wp_plugin_template_%s_section', self::POST_TYPE), __('Hall'), array(&$this, 'add_inner_meta_boxes'), self::POST_TYPE
             );
         }
 
@@ -106,7 +137,8 @@ if (!class_exists('Post_Type_Hall')) {
         /**
          * called off of the add meta box
          */
-        public function add_inner_meta_boxes($post) {
+        public function add_inner_meta_boxes($post)
+        {
             // Render the job order metabox
             include(sprintf("%s/../templates/%s_metabox.php", dirname(__FILE__), self::POST_TYPE));
         }

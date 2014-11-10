3b4781e967d6865ecd64b7ec7f22745abbd9cef1
elenastrizhack <elenastrizhack@gmail.com>
2014-11-11 01:48:09 +0200

    confirm

--- js/kivi_schedule_main.js
+++ js/kivi_schedule_main.js
@@ -26,17 +26,17 @@ jQuery(document).ready(function($) {
     });
 
     $('#advanced-sortables .inside').append('<div id="ajaxBusy"><img src="' + img_path.template_url + '/ajax-loader.gif"></div>');
-    
+
     $('#select_cities').change(function() {
         var city_id = $(this).find('option:selected').val();
         $('#ajaxBusy').css('display', 'block');
         $('.schedule-city, .schedule-club-name, .hall-schedule ').css('display', 'block');
         var city_blocks = $('#kivischedule .schedule-city');
-        $.each(city_blocks, function(){
-            if($(this).attr('data-city-id') == city_id){
+        $.each(city_blocks, function() {
+            if ($(this).attr('data-city-id') == city_id) {
                 $(this).css('display', 'block')
-            } else{
-                $(this).css('display','none');
+            } else {
+                $(this).css('display', 'none');
             }
         });
         $.ajax({
@@ -74,12 +74,12 @@ jQuery(document).ready(function($) {
         $('#ajaxBusy').css('display', 'block');
         $('.schedule-club-name, .hall-schedule ').css('display', 'block');
         var club_id = $(this).find('option:selected').val();
-              var club_blocks = $('#kivischedule .schedule-club-name');
-        $.each(club_blocks, function(){
-            if($(this).attr('data-club-id') == club_id){
+        var club_blocks = $('#kivischedule .schedule-club-name');
+        $.each(club_blocks, function() {
+            if ($(this).attr('data-club-id') == club_id) {
                 $(this).css('display', 'block')
-            } else{
-                $(this).css('display','none');
+            } else {
+                $(this).css('display', 'none');
             }
         });
         $.ajax({
@@ -118,12 +118,12 @@ jQuery(document).ready(function($) {
     $('#select_halls').change(function() {
         $('.hall-schedule ').css('display', 'block');
         var hall_id = $(this).find('option:selected').val();
-           var city_blocks = $('#kivischedule .hall-schedule');
-        $.each(city_blocks, function(){
-            if($(this).attr('data-hall-id') == hall_id){
+        var city_blocks = $('#kivischedule .hall-schedule');
+        $.each(city_blocks, function() {
+            if ($(this).attr('data-hall-id') == hall_id) {
                 $(this).css('display', 'block')
-            } else{
-                $(this).css('display','none');
+            } else {
+                $(this).css('display', 'none');
             }
         });
         $.ajax({
@@ -190,7 +190,7 @@ jQuery(document).ready(function($) {
         var sorting = [[0, 0]];
         $table.trigger('sorton', [sorting]);
         $table.find('.headerSortDown').trigger('click');
-        
+
         $.ajax({
             type: "POST",
             data: {
@@ -223,27 +223,29 @@ jQuery(document).ready(function($) {
     }
     $('.delete-schedule-row').click(
             function() {
-                console.log('here');
-                var current_row = $(this).parents('tr');
-                var schedule_id = current_row.attr('data-schedule-id');
-                current_row.remove();
-                $.ajax({
-                    type: "POST",
-                    data: {
-                        action: 'remove_schedule',
-                        schedule_id: schedule_id
-                    },
-                    url: ajaxurl,
-                    error: function(jqXHR, textStatus, errorThrown, response) {
-                        console.log(jqXHR);
-                        console.log(textStatus);
-                        console.log(errorThrown);
-                        console.log(response);
-                    },
-                    success: function(data) {
-                        console.log(data);
-                    }
-                });
+                if (confirm('Вы действительно желаете удалить расписание?')) {
+                    console.log('here');
+                    var current_row = $(this).parents('tr');
+                    var schedule_id = current_row.attr('data-schedule-id');
+                    current_row.remove();
+                    $.ajax({
+                        type: "POST",
+                        data: {
+                            action: 'remove_schedule',
+                            schedule_id: schedule_id
+                        },
+                        url: ajaxurl,
+                        error: function(jqXHR, textStatus, errorThrown, response) {
+                            console.log(jqXHR);
+                            console.log(textStatus);
+                            console.log(errorThrown);
+                            console.log(response);
+                        },
+                        success: function(data) {
+                            console.log(data);
+                        }
+                    });
+                }
             }
     )
     $('#add-new-chedule-row').click(function() {
